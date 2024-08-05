fca1a2444a530bc3f24b1be2fb95deee118e48f3
rykuno <rykuno@gmail.com>
2024-08-05 07:22:46 -0500

    updated mailer configs

---
+++ src/lib/server/api/interfaces/email.interface.ts
@@ -0,0 +1,7 @@
+
+
+
+export interface Email {
+  subject(): string
+  html(): string;
+}
--- src/lib/server/api/services/mailer.service.ts
+++ src/lib/server/api/services/mailer.service.ts
@@ -1,104 +1,44 @@
-import fs from 'fs';
-import path from 'path';
 import nodemailer from 'nodemailer';
-import handlebars from 'handlebars';
-import { fileURLToPath } from 'url';
 import { injectable } from 'tsyringe';
+import type { Email } from '../interfaces/email.interface';
+import { config } from '../common/config';
 
-/* -------------------------------------------------------------------------- */
-/*                                   Service                                  */
-/* -------------------------------------------------------------------------- */
-/* -------------------------------------------------------------------------- */
-/* ---------------------------------- About --------------------------------- */
-/*
-Services are responsible for handling business logic and data manipulation. 
-They genreally call on repositories or other services to complete a use-case.
-*/
-/* ---------------------------------- Notes --------------------------------- */
-/*
-Services should be kept as clean and simple as possible. 
 
-Create private functions to handle complex logic and keep the public methods as 
-simple as possible. This makes the service easier to read, test and understand.
-*/
-/* -------------------------------------------------------------------------- */
-
-type SendMail = {
+type SendProps = {
 	to: string | string[];
-	subject: string;
-	html: string;
-};
-
-type SendTemplate<T> = {
-	to: string | string[];
-	props: T;
-};
+	email: Email;
+}
 
 @injectable()
 export class MailerService {
-	private nodemailer = nodemailer.createTransport({
-		host: 'smtp.ethereal.email',
-		port: 587,
-		secure: false, // Use `true` for port 465, `false` for all other ports
-		auth: {
-			user: 'adella.hoppe@ethereal.email',
-			pass: 'dshNQZYhATsdJ3ENke'
-		}
-	});
-
-	sendEmailVerificationToken(data: SendTemplate<{ token: string }>) {
-		const template = handlebars.compile(this.getTemplate('email-verification-token'));
-		return this.send({
-			to: data.to,
-			subject: 'Email Verification',
-			html: template({ token: data.props.token })
-		});
-	}
-
-	sendEmailChangeNotification(data: SendTemplate<null>) {
-		const template = handlebars.compile(this.getTemplate('email-change-notice'));
-		return this.send({
-			to: data.to,
-			subject: 'Email Change Notice',
-			html: template(null)
-		});
-	}
-
-	sendLoginRequest(data: SendTemplate<{ token: string }>) {
-		const template = handlebars.compile(this.getTemplate('email-verification-token'));
-		return this.send({
-			to: data.to,
-			subject: 'Login Request',
-			html: template({ token: data.props.token })
-		});
-	}
-
-	sendWelcome(data: SendTemplate<null>) {
-		const template = handlebars.compile(this.getTemplate('welcome'));
-		return this.send({
-			to: data.to,
-			subject: 'Welcome!',
-			html: template(null)
-		});
-	}
-
-	private async send({ to, subject, html }: SendMail) {
-		const message = await this.nodemailer.sendMail({
-			from: '"Example" <example@ethereal.email>', // sender address
+	private async sendDev({ to, email }: SendProps) {
+		const message = await nodemailer.createTransport({
+			host: 'smtp.ethereal.email',
+			port: 587,
+			secure: false, // Use `true` for port 465, `false` for all other ports
+			auth: {
+				user: 'adella.hoppe@ethereal.email',
+				pass: 'dshNQZYhATsdJ3ENke'
+			}
+		}).sendMail({
+			from: '"Example" <example@ethereal.email>',
 			bcc: to,
-			subject, // Subject line
-			text: html,
-			html
+			subject: email.subject(),
+			text: email.html(),
+			html: email.html()
 		});
 		console.log(nodemailer.getTestMessageUrl(message));
 	}
 
-	private getTemplate(template: string) {
-		const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-		const __dirname = path.dirname(__filename); // get the name of the directory
-		return fs.readFileSync(
-			path.join(__dirname, `../infrastructure/email-templates/${template}.hbs`),
-			'utf-8'
-		);
+	private async sendProd({ to, email }: SendProps) {
+		// CONFIGURE MAILER
+	}
+
+	async send({ to, email }: SendProps) {
+		if (config.isProduction) {
+			await this.sendProd({ to, email });
+		} else {
+			await this.sendDev({ to, email });
+		}
 	}
 }
